cbcbd578a0643cbd4a370eb95b403bb5165a3417
KiwiiXaos <43905857+KiwiiXaos@users.noreply.github.com>
2020-12-19 01:30:47 +0100

    Add files via upload

---
+++ Assets/spawnobject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnobject : MonoBehaviour
+{
+
+    public int difficulty;
+    public Transform[] SpawnPoints;
+    public GameObject[] ElementSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Spawn()
+    {
+        Debug.Log("spawn");
+        for(int i = 0; i < difficulty; i++)
+        {
+            Debug.Log("bubububub");
+            int type = Random.Range(0, ElementSpawn.Length);
+            int coordspawn = Random.Range(0, SpawnPoints.Length - 1);
+            float x = Random.Range(-9.17f, 9.09f);
+            float z = 5;
+            float y = Random.Range(-4.75f, 4.75f);
+            Vector3 pos = new Vector3(x, y, z);
+            Debug.Log(type);
+            Instantiate(this.ElementSpawn[type], pos, Quaternion.identity);
+            
+        }
+        
+
+    }
+}
