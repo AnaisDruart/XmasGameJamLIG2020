9034edda0f5ea4eb7ca36fc0b4680378da727412
Anaïs DRUART <43911776+Anasarasa@users.noreply.github.com>
2020-12-19 20:12:30 +0100

    Add files via upload

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private NetworkManagerLobby networkManager = null;
+
+    [Header("UI")]
+    [SerializeField] private GameObject landingPagePanel = null;
+
+    public void HostLobby() // When we press the host button
+    {
+        networkManager.StartHost();
+
+        landingPagePanel.SetActive(false); //Disable the landing page panel
+    }
+}
